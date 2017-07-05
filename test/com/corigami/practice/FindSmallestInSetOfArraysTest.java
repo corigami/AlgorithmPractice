3b8331029228662e086ad42b0fe0ad812a738537
Corey Willinger <corey.willinger@gmail.com>
2017-07-05 20:52:41 +0430

    chore: updated comments.

--- test/com/corigami/practice/FindSmallestInSetOfArraysTest.java
+++ test/com/corigami/practice/FindSmallestInSetOfArraysTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
+ * Basic unit tests.  Checks for positive and negative responses.  Assumes all ints in the arrays
+ * are positive.  Also tests correct handling of array of different lengths.
  * Created by Corey Willinger on 7/5/2017.
  */
 class FindSmallestInSetOfArraysTest {
@@ -24,10 +26,11 @@ class FindSmallestInSetOfArraysTest {
         testArray2 = new MyArray(new int[]{1,3,9,12,15,18});
         testArray3 = new MyArray(new int[]{0,1,4,8,12,16,20,24,28,32,36,40,44});
         testArray4 = new MyArray(new int[]{0, 1, 3, 5, 7, 9});
-
     }
 
-
+    /**
+     * Checks for the known match of 12 in arrays 1,2, and 3.
+     */
     @Test
     @DisplayName("Test for match")
     void testForKnownMatch() {
@@ -36,6 +39,10 @@ class FindSmallestInSetOfArraysTest {
         assertEquals(12, testSearch.searchForSmallest());
     }
 
+
+    /**
+     * Verifies the correct response that no match exists in arrays 1,2, and 4
+     */
     @Test
     @DisplayName("Test for noMatch")
     void testForNoMatch() {
